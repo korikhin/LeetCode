8d5f3966d12b634440aaa21f3c8289be7c70dcc8
Vladislav Korikhin <korikhin666@gmail.com>
2024-02-19 06:53:52 +0700

    feat: 6. Zigzag Conversion

--- Go/s0006/0006.go
+++ Go/s0006/0006.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Convert(s string, numRows int) string {
-	return ""
+	n := len(s)
+	if s == "" || numRows == 1 || numRows >= n {
+		return s
+	}
+
+	d := make([]strings.Builder, numRows)
+	for _, b := range d {
+		b.Grow(n/numRows + 1)
+	}
+
+	j, delta := 0, -1
+	for _, r := range s {
+		if j == 0 || j == numRows-1 {
+			delta *= -1
+		}
+		d[j].WriteRune(r)
+		j += delta
+	}
+
+	var z strings.Builder
+	z.Grow(n)
+	for _, b := range d {
+		z.WriteString(b.String())
+	}
+	return z.String()
+}
+
+func main() {
+	s := "PAYPALISHIRING"
+	n := 3
+
+	fmt.Println(Convert(s, n))
 }
