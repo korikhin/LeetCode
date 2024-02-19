0e33fa3d6a339ca4e25cf4ad6213df7f03732b34
Vladislav Korikhin <korikhin666@gmail.com>
2024-02-19 07:36:40 +0700

    feat: 6. Zigzag Conversion

--- Go/s0006/0006.go
+++ Go/s0006/0006.go
@@ -1,32 +1,25 @@
 package main
 
-import "strings"
-
 func Convert(s string, numRows int) string {
 	n := len(s)
 	if numRows == 1 || numRows >= n {
 		return s
 	}
 
-	b := make([]strings.Builder, numRows)
-	c := 2*(n-1)/(2*numRows-2) + 1
-	for i := range b {
-		b[i].Grow(c)
-	}
+	z := make([]byte, n)
+	step, count := (numRows-1)<<1, 0
 
-	i, delta := 0, -1
-	for _, r := range s {
-		if i == 0 || i == numRows-1 {
-			delta *= -1
+	for i := 0; i < numRows; i++ {
+		for j := 0; j+i < n; j += step {
+			z[count] = s[i+j]
+			count++
+
+			if i != 0 && i != numRows-1 && j+step-i < n {
+				z[count] = s[j+step-i]
+				count++
+			}
 		}
-		b[i].WriteRune(r)
-		i += delta
 	}
 
-	var z strings.Builder
-	z.Grow(n)
-	for _, bb := range b {
-		z.WriteString(bb.String())
-	}
-	return z.String()
+	return string(z)
 }
