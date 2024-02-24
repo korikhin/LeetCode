6ef363ef47b523d66e5cc6b73c228eb08bc17805
Vladislav Korikhin <korikhin666@gmail.com>
2024-02-25 02:21:22 +0700

    feat: 26. Remove Duplicates from Sorted Array

--- solutions/s0026/0026.go
+++ solutions/s0026/0026.go
@@ -1,6 +1,17 @@
 package main
 
-// TODO: implement
 func RemoveDuplicates(nums []int) int {
-	return 0
+	if len(nums) < 2 {
+		return len(nums)
+	}
+
+	i := 0
+	for k := 1; k < len(nums); k++ {
+		if nums[k] != nums[i] {
+			i++
+			nums[i] = nums[k]
+		}
+	}
+
+	return i + 1
 }
