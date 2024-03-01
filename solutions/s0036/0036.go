236b8077ed4d556ea61dd65e998b0d08e96a8fb2
Vladislav Korikhin <korikhin666@gmail.com>
2024-03-01 13:10:13 +0700

    feat: 36

--- solutions/s0036/0036.go
+++ solutions/s0036/0036.go
@@ -1,5 +1,28 @@
 package main
 
 func IsValidSudoku(board [][]byte) bool {
-	return false
+	rows := make([]int, 9)
+	cols := make([]int, 9)
+	grid := make([]int, 9)
+
+	for i, row := range board {
+		for j, cell := range row {
+			if cell < '1' || cell > '9' {
+				continue
+			}
+
+			n := 3*(i/3) + (j / 3)
+			bit := 1 << (cell - '1')
+
+			if bit&(rows[i]|cols[j]|grid[n]) != 0 {
+				return false
+			}
+
+			rows[i] |= bit
+			cols[j] |= bit
+			grid[n] |= bit
+		}
+	}
+
+	return true
 }
