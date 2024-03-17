833e27308911da25e35bb2276b21f7e81b60ead5
Vladislav Korikhin <korikhin666@gmail.com>
2024-03-18 04:31:41 +0700

    feat: 190

--- solutions/s0190/0190.go
+++ solutions/s0190/0190.go
@@ -9,3 +9,12 @@ func ReverseBits(num uint32) uint32 {
 
 	return num
 }
+
+// func ReverseBits(num uint32) uint32 {
+// 	result := uint32(0)
+// 	for i := 0; i < 32; i++ {
+// 		result |= (num & 1) << (31 - i)
+// 		num >>= 1
+// 	}
+// 	return result
+// }
