52bfce200c5c79c2d73076e78d58458d2cc9d74d
Vladislav <korikhin666@gmail.com>
2024-05-01 20:25:04 +0700

    104

--- solutions/0463/main.go
+++ solutions/0463/main.go
@@ -10,16 +10,19 @@ func islandPerimeter(grid [][]int) int {
 
 	for i := 0; i < len(grid); i++ {
 		for k := 0; k < r; k++ {
-			if grid[i][k] == 1 {
-				p += 4
-
-				if i > 0 && grid[i-1][k] == 1 {
-					p -= 2
-				}
-				if k > 0 && grid[i][k-1] == 1 {
-					p -= 2
-				}
+			if grid[i][k] != 1 {
+				continue
 			}
+
+			p += 4
+
+			if i > 0 && grid[i-1][k] == 1 {
+				p -= 2
+			}
+			if k > 0 && grid[i][k-1] == 1 {
+				p -= 2
+			}
+
 		}
 	}
 
