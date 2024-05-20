025a35142b68f80c4e0f8ad865a27724d4a9c0cd
Vladislav <korikhin666@gmail.com>
2024-05-21 02:55:21 +0700

    chore(128): denest, alloc

--- solutions/0128/main.go
+++ solutions/0128/main.go
@@ -1,24 +1,25 @@
 package main
 
-func longestConsecutive(nums []int) int {
-	m := make(map[int]struct{})
+func longestConsecutive(nums []int) (r int) {
+	m := make(map[int]struct{}, len(nums))
 	for _, val := range nums {
 		m[val] = struct{}{}
 	}
 
-	r := 0
 	for n := range m {
-		if _, exists := m[n-1]; !exists {
-			i := 1
-			for {
-				if _, exists := m[n+i]; !exists {
-					break
-				}
-				i++
+		if _, exists := m[n-1]; exists {
+			continue
+		}
+
+		i := 1
+		for {
+			if _, exists := m[n+i]; !exists {
+				break
 			}
-			if i > r {
-				r = i
-			}
+			i++
+		}
+		if i > r {
+			r = i
 		}
 	}
 
