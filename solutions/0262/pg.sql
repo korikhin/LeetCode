8113c3cd1d80e50206beb22f818cafe8b948132f
Vladislav Korikhin <korikhin666@gmail.com>
2024-12-25 20:33:16 +0700

    perf(262): important join tweak

--- solutions/0262/pg.sql
+++ solutions/0262/pg.sql
@@ -8,10 +8,12 @@ select
             end) / count(*), 2) as "Cancellation Rate"
 from
     trips t
-    join users uc on t.client_id = uc.users_id
-        and uc.banned = 'No'
+    -- Join with drivers first due to much smaller cardinality
+    -- (or use LATERAL for explicit handling)
     join users ud on t.driver_id = ud.users_id
         and ud.banned = 'No'
+    join users uc on t.client_id = uc.users_id
+        and uc.banned = 'No'
 where
     t.request_at between '2013-10-01' and '2013-10-03'
 group by
